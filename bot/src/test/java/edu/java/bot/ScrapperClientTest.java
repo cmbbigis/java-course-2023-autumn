b9f103046b93675a133d698f035f58c8b76bd8d1
cmbbigis <gavrilov.nikita@urfu.me>
2024-03-03 23:10:51 +0500

    done

---
+++ bot/src/test/java/edu/java/bot/ScrapperClientTest.java
@@ -0,0 +1,30 @@
+package edu.java.bot;
+
+import edu.java.bot.api.ScrapperClient;
+import edu.java.bot.api.request.LinkUpdate;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+public class ScrapperClientTest {
+
+    @Test
+    public void testPostUpdate() throws URISyntaxException {
+        ScrapperClient client = new ScrapperClient("http://localhost:8090");
+
+        LinkUpdate update = new LinkUpdate();
+        update.setId(1);
+        update.setUrl(new URI("http://example.com"));
+        update.setDescription("test");
+        update.setTgChatIds(Arrays.asList(1L, 2L, 3L));
+
+        Mono<LinkUpdate> result = client.postUpdate(update);
+
+        StepVerifier.create(result)
+            .expectComplete()
+            .verify();
+    }
+}
