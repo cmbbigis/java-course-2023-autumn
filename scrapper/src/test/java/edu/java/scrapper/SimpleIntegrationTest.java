3e2735d60c70b3a41529e3088cf6d3c4cced9b01
cmbbigis <gavrilov.nikita@urfu.me>
2024-05-05 18:24:58 +0500

    refactor

--- scrapper/src/test/java/edu/java/scrapper/SimpleIntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/SimpleIntegrationTest.java
@@ -2,12 +2,10 @@ package edu.java.scrapper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class SimpleIntegrationTest extends IntegrationTest {
 
     @Test
