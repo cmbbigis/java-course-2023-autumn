99c606375980601bb1d5e62c8003224029119799
cmbbigis <gavrilov.nikita@urfu.me>
2024-05-05 18:24:58 +0500

    refactor

--- scrapper/src/test/java/edu/java/scrapper/BotClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/BotClientTest.java
@@ -3,17 +3,18 @@ package edu.java.scrapper;
 import edu.java.api.BotClient;
 import edu.java.api.request.LinkUpdate;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 
-public class BotClientTest {
 
+public class BotClientTest {
     @Test
     public void testPostUpdate() throws URISyntaxException {
-        BotClient client = new BotClient("http://localhost:8090");
+        BotClient client = Mockito.mock(BotClient.class);
 
         LinkUpdate update = new LinkUpdate();
         update.setId(1);
@@ -21,10 +22,12 @@ public class BotClientTest {
         update.setDescription("test");
         update.setTgChatIds(Arrays.asList(1L, 2L, 3L));
 
+        Mockito.when(client.postUpdate(update)).thenReturn(Mono.just(update));
+
         Mono<LinkUpdate> result = client.postUpdate(update);
 
         StepVerifier.create(result)
-            .expectComplete()
-            .verify();
+            .expectNextMatches(response -> response.getId() == 1)
+            .verifyComplete();
     }
 }
