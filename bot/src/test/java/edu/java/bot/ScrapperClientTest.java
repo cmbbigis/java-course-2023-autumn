fe96ad20d7e1922faa02b5c7b5c338313bf70d05
cmbbigis <gavrilov.nikita@urfu.me>
2024-05-05 18:24:58 +0500

    refactor

--- bot/src/test/java/edu/java/bot/ScrapperClientTest.java
+++ bot/src/test/java/edu/java/bot/ScrapperClientTest.java
@@ -6,14 +6,16 @@ import edu.java.bot.api.request.RemoveLinkRequest;
 import edu.java.bot.api.response.LinkResponse;
 import edu.java.bot.api.response.ListLinksResponse;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import java.util.Collections;
 
 public class ScrapperClientTest {
-
     @Test
     public void testRegisterChat() {
-        ScrapperClient client = new ScrapperClient("http://localhost:8080");
+        ScrapperClient client = Mockito.mock(ScrapperClient.class);
+        Mockito.when(client.registerChat(12345L)).thenReturn(Mono.empty());
 
         Mono<Void> result = client.registerChat(12345L);
 
@@ -24,7 +26,8 @@ public class ScrapperClientTest {
 
     @Test
     public void testDeleteChat() {
-        ScrapperClient client = new ScrapperClient("http://localhost:8080");
+        ScrapperClient client = Mockito.mock(ScrapperClient.class);
+        Mockito.when(client.deleteChat(12345L)).thenReturn(Mono.empty());
 
         Mono<Void> result = client.deleteChat(12345L);
 
@@ -35,7 +38,10 @@ public class ScrapperClientTest {
 
     @Test
     public void testGetAllLinks() {
-        ScrapperClient client = new ScrapperClient("http://localhost:8080");
+        ScrapperClient client = Mockito.mock(ScrapperClient.class);
+        ListLinksResponse mockResponse = new ListLinksResponse();
+        mockResponse.setLinks(Collections.singletonList(new LinkResponse()));
+        Mockito.when(client.getAllLinks(12345L)).thenReturn(Mono.just(mockResponse));
 
         Mono<ListLinksResponse> result = client.getAllLinks(12345L);
 
@@ -46,10 +52,12 @@ public class ScrapperClientTest {
 
     @Test
     public void testAddLink() {
-        ScrapperClient client = new ScrapperClient("http://localhost:8080");
-
+        ScrapperClient client = Mockito.mock(ScrapperClient.class);
         AddLinkRequest request = new AddLinkRequest();
         request.setLink("http://example.com");
+        LinkResponse mockResponse = new LinkResponse();
+        mockResponse.setId(1);
+        Mockito.when(client.addLink(12345L, request)).thenReturn(Mono.just(mockResponse));
 
         Mono<LinkResponse> result = client.addLink(12345L, request);
 
@@ -60,10 +68,12 @@ public class ScrapperClientTest {
 
     @Test
     public void testRemoveLink() {
-        ScrapperClient client = new ScrapperClient("http://localhost:8080");
-
+        ScrapperClient client = Mockito.mock(ScrapperClient.class);
         RemoveLinkRequest request = new RemoveLinkRequest();
         request.setLink("http://example.com");
+        LinkResponse mockResponse = new LinkResponse();
+        mockResponse.setId(1);
+        Mockito.when(client.removeLink(12345L, request)).thenReturn(Mono.just(mockResponse));
 
         Mono<LinkResponse> result = client.removeLink(12345L, request);
 
